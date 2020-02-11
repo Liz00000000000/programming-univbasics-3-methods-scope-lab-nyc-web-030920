04cd260287e7f34de942089af3c2adf7c66c0621
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-11 22:40:38 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,7 +6,7 @@ end
 #status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def toadstool
-status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
